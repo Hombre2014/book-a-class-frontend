25c49fd9f12d213dae1e8f7e4e74e82006b084e4
Hombre2014 <chamkoriyski@gmail.com>
2022-05-13 14:59:43 +0200

    Add Courses file to Home page and modify single course display

--- src/features/courses/Course.jsx
+++ src/features/courses/Course.jsx
@@ -1,42 +1,19 @@
-import PropTypes from 'prop-types';
-
-const Course = (props) => {
+const Course = (course) => {
   const {
-    category, title, description, duration, photo, price,
-  } = props;
+    id, category, title, description, duration, photo, price,
+  } = course;
 
   return (
-    <div>
-      <div className="row">
-        <div className="col-8">{title}</div>
-      </div>
-      <div className="row">
-        <div className="col-8">{description}</div>
-      </div>
-      <div className="row">
-        <div className="col-8">{category}</div>
-      </div>
-      <div className="row">
-        <div className="col-8">{duration}</div>
-      </div>
-      <div className="row">
-        <div className="col-8">{photo}</div>
-      </div>
-      <div className="row">
-        <div className="col-8">{price}</div>
-      </div>
+    <div className="row">
+      <div className="col-8">{id}</div>
+      <div className="col-8">{title}</div>
+      <div className="col-8">{photo}</div>
+      <div className="col-8">{category}</div>
+      <div className="col-8">{description}</div>
+      <div className="col-8">{duration}</div>
+      <div className="col-8">{price}</div>
     </div>
   );
-};
-
-Course.propTypes = {
-  // id: PropTypes.number.isRequired,
-  category: PropTypes.string.isRequired,
-  title: PropTypes.string.isRequired,
-  description: PropTypes.string.isRequired,
-  duration: PropTypes.number.isRequired,
-  photo: PropTypes.string.isRequired,
-  price: PropTypes.number.isRequired,
 };
 
 export default Course;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import SideBar from '../components/DesktopNav';
 import HamBurger from '../components/mobileNav';
+import Courses from '../features/courses/Courses';
 
 const Home = () => (
   <div className="h-screen flex overflow-hidden bg-white">
@@ -11,7 +12,7 @@ const Home = () => (
     <div className="flex flex-col min-w-0 flex-1 overflow-hidden mt-32 md:mt-1">
       {/* Navigation for mobile */}
       <HamBurger />
-      Hi there good to see you!
+      <Courses />
     </div>
   </div>
 );
