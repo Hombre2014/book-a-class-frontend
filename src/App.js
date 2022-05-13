75eac8721102081a6c131c1ed5c1c6be5af84af4
KayLemba <kaylemba@yahoo.com>
2022-05-13 11:03:05 +0200

    Add Desktop and mobile navigation

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Courses from './features/courses/Courses';
+import Home from './pages/Home';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/" element={<Courses />} />
       </Routes>
     </BrowserRouter>
