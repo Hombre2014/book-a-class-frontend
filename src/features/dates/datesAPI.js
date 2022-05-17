9a4a0caa9820d787fa60793b8dc0d8d5662b2c11
Hombre2014 <chamkoriyski@gmail.com>
2022-05-17 08:59:52 +0200

    Fix datesAPI

--- src/features/dates/datesAPI.js
+++ src/features/dates/datesAPI.js
@@ -1,6 +1,22 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
 const API_URL = 'http://localhost:3000/api/v1/courses';
+
+const { id } = useParams();
+const [course, setCourse] = useState({});
+
+const fetchCourse = async () => {
+  const course = await fetch(`http://localhost:3000/api/v1/courses/${id}/start_dates`);
+  const res = await course.json();
+  setCourse(res);
+  return res;
+};
+
+useEffect(() => {
+  fetchCourse();
+}, []);
 
 const fetchDates = createAsyncThunk(
   'dates/fetchDates',
