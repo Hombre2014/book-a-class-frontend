2bd4d617c62704f66bc64834616d34f1178226cf
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2022-05-17 17:21:00 +0200

    Move the Reserve button and add some stylings

--- src/features/courses/Course.jsx
+++ src/features/courses/Course.jsx
@@ -1,3 +1,4 @@
+import { useParams, useNavigate } from 'react-router-dom';
 import './course.css';
 
 const Course = (course) => {
@@ -5,11 +6,14 @@ const Course = (course) => {
     title, category, description, duration, photo, price,
   } = course;
 
+  const { id } = useParams();
+  const navigate = useNavigate();
+
   return (
     <div className="course-details">
       <div className="row">
         <img className="col-8" src={photo} alt="Web" />
-        <div className="col-4">
+        <div className="col-4 con-details">
           <h2 className="row h2">{title}</h2>
           <h3 className="row h3">{category}</h3>
           <p className="row">{description}</p>
@@ -17,12 +21,16 @@ const Course = (course) => {
             Duration:
             &nbsp;
             {duration}
+            &nbsp;
+            hours
           </span>
           <span className="row">
             Price:
             &nbsp;
+            $
             {price}
           </span>
+          <button className="btn btn-reserve" type="button" value={id} onClick={() => navigate(`/details/${id}/reserve`)}>Reserve</button>
         </div>
       </div>
     </div>
