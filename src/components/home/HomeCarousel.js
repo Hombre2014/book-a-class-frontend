63047bd03ac060f61f7592a41ea9b09e5ca1c74e
KayLemba <kaylemba@yahoo.com>
2022-05-18 11:28:23 +0200

    Add Carousel

---
+++ src/components/home/HomeCarousel.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import ClassCard from './ClassCard';
+
+const HomeCarousel = () => {
+  const scrollLeft = () => {
+    document.getElementById('home-carousel').scrollLeft += 500;
+  };
+
+  const scrollRight = () => {
+    document.getElementById('home-carousel').scrollLeft -= 500;
+  };
+
+  return (
+    <>
+      <div className="home-carousel-btns">
+        <button type="button" onClick={scrollRight}>
+          ᐊ
+        </button>
+        <button type="button" onClick={scrollLeft}>
+          ᐅ
+        </button>
+      </div>
+      <div id="home-carousel" className="home-carousel">
+        <ClassCard />
+        <ClassCard />
+        <ClassCard />
+        <ClassCard />
+        <ClassCard />
+      </div>
+    </>
+  );
+};
+
+export default HomeCarousel;
