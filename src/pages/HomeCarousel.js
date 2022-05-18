5fe15a24f6ddb3b63073886f51c8795d2a764af4
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2022-05-18 13:37:56 +0200

    Changing the carousel home page

--- src/pages/HomeCarousel.js
+++ src/pages/HomeCarousel.js
@@ -1,13 +1,13 @@
-// import React from 'react';
-// import Menu from './Menu';
-// import HomeContent from '../components/home/HomeContent';
-// import '../stylesheets/home.css';
+import React from 'react';
+import Menu from './Menu';
+import HomeContent from '../components/home/HomeContent';
+import '../stylesheets/home.css';
 
-// const Home = () => (
-//   <div className="flex">
-//     <Menu />
-//     <HomeContent />
-//   </div>
-// );
+const Home = () => (
+  <div className="flex">
+    <Menu />
+    <HomeContent />
+  </div>
+);
 
-// export default Home;
+export default Home;
