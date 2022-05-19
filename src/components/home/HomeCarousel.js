c51900649d71ae0d0d4d30de324b49b5d3319fc5
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2022-05-19 09:45:25 +0200

    Implementing the carousel. Needs fix

--- src/components/home/HomeCarousel.js
+++ src/components/home/HomeCarousel.js
@@ -1,7 +1,21 @@
-import React from 'react';
-import ClassCard from './ClassCard';
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+// import ClassCard from './ClassCard';
+import ShortCourse from '../../features/courses/ShortCourse';
+import fetchCourses from '../../features/courses/courseAPI';
 
 const HomeCarousel = () => {
+  const courses = useSelector((state) => state.courses);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    dispatch(fetchCourses());
+  }, [dispatch]);
+
+  console.log(courses);
+
   const scrollLeft = () => {
     document.getElementById('home-carousel').scrollLeft += 500;
   };
@@ -21,14 +35,22 @@ const HomeCarousel = () => {
         </button>
       </div>
       <div id="home-carousel" className="home-carousel">
-        <ClassCard />
-        <ClassCard />
-        <ClassCard />
-        <ClassCard />
-        <ClassCard />
-        <ClassCard />
-        <ClassCard />
-        <ClassCard />
+        {courses.courses && courses.courses.length > 0 && courses.courses.map((course) => (
+          <div className="class-card" key={course.id}>
+            <div className="key">
+              <button type="button" value={course.id} onClick={() => navigate(`/details/${course.id}`)}>
+                {/* <ClassCard> */}
+                <ShortCourse
+                  photo={course.photo}
+                  title={course.title}
+                  category={course.category}
+                  description={course.description}
+                />
+                {/* </ClassCard> */}
+              </button>
+            </div>
+          </div>
+        ))}
       </div>
     </>
   );
